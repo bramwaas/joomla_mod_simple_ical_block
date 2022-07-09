166cb97e53eb9fd2325520b3d5798996c85458c3
ahcwa <ahcwa@bram-len.fritz.box>
2022-07-09 19:11:37 +0200

    0.0.0 removed admin and sql everythin now in site

--- sql/updates/mysql/0.0.2.sql
+++
@@ -1,7 +0,0 @@
---
--- This file will contain Table structure changes for `#__simpleicalblock`
--- v 0.0.2 
---
--- ALTER TABLE `#__simpleicalblock` ADD `testblob` MEDIUMBLOB NULL AFTER `alias`;
--- ALTER TABLE `#__simpleicalblock` ADD `transient_expires` datetime; 
-
