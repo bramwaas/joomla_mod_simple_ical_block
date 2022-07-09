ccf884b699fcff81c7e81378c1b4a1ff45dd948f
ahcwa <ahcwa@bram-len.fritz.box>
2022-07-09 15:58:18 +0200

    0.0.0 sql out admin dir site langusage off

---
+++ sql/updates/mysql/0.0.2.sql
@@ -0,0 +1,7 @@
+--
+-- This file will contain Table structure changes for `#__simpleicalblock`
+-- v 0.0.2 
+--
+-- ALTER TABLE `#__simpleicalblock` ADD `testblob` MEDIUMBLOB NULL AFTER `alias`;
+-- ALTER TABLE `#__simpleicalblock` ADD `transient_expires` datetime; 
+
